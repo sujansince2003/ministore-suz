a17d967d85562f42cd8bd7dea1c48d9fb8129ac0
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-09-15 01:12:01 +0545

    build the utils function: truncate; to truncate long texts for toast msg formatCurreny to formate the currency accoring to countries

---
+++ lib/products-utils.ts
@@ -0,0 +1,16 @@
+function formatCurrency(amount: number, currency: string = 'USD'): string {
+    return new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency,
+    }).format(amount);
+}
+
+
+function truncateText(text: string, maxLength: number): string {
+    if (text.length <= maxLength) {
+        return text;
+    }
+    return text.slice(0, maxLength) + '...';
+}
+
+export { formatCurrency, truncateText };
