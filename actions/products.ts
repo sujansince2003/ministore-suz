634a92a671c68d40e56149366285fe532f928c72
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-09-15 01:08:39 +0545

    defined server actions to get Product detail, get Categories ,get products by category

--- actions/products.ts
+++ actions/products.ts
@@ -1,15 +1,37 @@
 "use server"
 
+import type { ProductType } from "@/types/index.types";
 
-export async function getProducts() {
+export async function getProducts(): Promise<ProductType[]> {
 
     const res = await fetch("https://fakestoreapi.com/products", {
         cache: "no-store"
     });
     const data = await res.json();
+    return data;
+}
 
 
+export async function getProduct(id: string): Promise<ProductType> {
+    const res = await fetch(`https://fakestoreapi.com/products/${id}`, {
+        cache: "no-store"
+    });
+    const data = await res.json();
+    return data;
+}
 
+export async function getCategories(): Promise<string[]> {
+    const res = await fetch("https://fakestoreapi.com/products/categories", {
+        cache: "no-store"
+    });
+    const data = await res.json();
+    return data;
+}
 
+export async function getProductsByCategory(category: string): Promise<ProductType[]> {
+    const res = await fetch(`https://fakestoreapi.com/products/category/${category}`, {
+        cache: "no-store"
+    });
+    const data = await res.json();
     return data;
 }
