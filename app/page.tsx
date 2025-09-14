8610afb06f6c3a49be35eee6f4085846827b0b3b
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-09-14 21:30:00 +0545

    build a simple product catalog to show products details with product card

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,19 @@
-export default function Home() {
-  return <div>mini store</div>;
+import { getProducts } from "@/actions/products";
+import ProductCard from "@/components/ui/general/ProductCard";
+import type { ProductType } from "@/types/index.types";
+
+export default async function Home() {
+  const products = await getProducts();
+
+  return (
+    <div className="flex flex-col gap-10 justify-center ">
+      <h1>Products page</h1>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {products?.map((product: ProductType) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+    </div>
+  );
 }
