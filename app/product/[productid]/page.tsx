8debc810b6ced7c6c5e64c15a83716f699fedeaf
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-09-15 01:24:42 +0545

    build the route page for product, fetched the product details and related products and called the ProductDetail component to render that data

---
+++ app/product/[productid]/page.tsx
@@ -0,0 +1,19 @@
+import { getProduct, getProductsByCategory } from "@/actions/products";
+import { ProductDetail } from "@/components/ui/general/ProductDetail";
+import { ProductType } from "@/types/index.types";
+import React from "react";
+
+const page = async ({ params }: { params: Promise<{ productid: string }> }) => {
+  const { productid } = await params;
+  console.log("Product ID:", productid);
+
+  const product = await getProduct(productid);
+  let relatedProducts: ProductType[] = [];
+  if (product) {
+    relatedProducts = await getProductsByCategory(product.category);
+  }
+
+  return <ProductDetail product={product} relatedProducts={relatedProducts} />;
+};
+
+export default page;
