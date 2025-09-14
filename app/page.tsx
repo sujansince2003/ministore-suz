0d87998bc816c3058eea1da4b4ddc727889e85e9
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-09-15 01:11:00 +0545

    show all products in the catalog page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,25 @@
 import { getProducts } from "@/actions/products";
+
 import ProductCard from "@/components/ui/general/ProductCard";
 import type { ProductType } from "@/types/index.types";
-
+import { CircleSlash2 } from "lucide-react";
 export default async function Home() {
   const products = await getProducts();
+  console.log(products);
+  if (products.length === 0) {
+    return (
+      <div className="text-center py-40">
+        <div className="mx-auto max-w-md">
+          <div className="mx-auto h-24 w-24 text-gray-300 dark:text-gray-600 mb-4">
+            <CircleSlash2 size={96} />
+          </div>
+          <h3 className="text-lg font-semibold text-gray-900 dark:text-gray-100">
+            No products found
+          </h3>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="flex flex-col gap-10 justify-center ">
