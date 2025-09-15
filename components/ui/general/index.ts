fe4575d331c9c304635b416386e8772ba2ecd33e
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-09-15 17:51:36 +0545

    barrel file for exporting custom general components

--- components/ui/general/index.ts
+++ components/ui/general/index.ts
@@ -1,4 +1,16 @@
+
+import CartItem from "./cartItem";
 import Header from "./Header";
+import HeaderCart from "./HeaderCart";
+import { ModeToggle } from "./ModeToggle";
+import ProductCard from "./ProductCard";
+import { ProductDetail } from "./ProductDetail";
+import { ProductFilters } from "./ProductFilters";
+import { ProductsWrapper } from "./ProductsWrapper";
+import ShowProducts from "./ShowProducts";
 import { ThemeProvider } from "./ThemeProvider";
 
-export { ThemeProvider, Header };
+
+
+
+export { ThemeProvider, Header, CartItem, HeaderCart, ModeToggle, ProductCard, ProductDetail, ProductFilters, ProductsWrapper, ShowProducts };
