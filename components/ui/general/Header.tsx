97f6cdd7524ab316c37c2921ebffab12b735a7fd
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-09-15 17:50:41 +0545

    updated the header with logo, updated theme toggle and added route to cart

--- components/ui/general/Header.tsx
+++ components/ui/general/Header.tsx
@@ -1,11 +1,20 @@
-import React from "react";
+import Link from "next/link";
+
 import { ModeToggle } from "./ModeToggle";
+import { Handbag } from "lucide-react";
+import HeaderCart from "./HeaderCart";
 
 const Header = () => {
   return (
-    <div className=" mx-auto max-w-6xl flex justify-between py-4 ">
-      <h1>MiniStore</h1>
-      <ModeToggle />
+    <div className=" mx-auto max-w-6xl flex justify-between py-4 px-4 sm:px-4 items-center">
+      <Link href="/" className="flex items-center gap-2">
+        <Handbag />
+        <h1 className="font-bold text-2xl">Suzz</h1>
+      </Link>
+      <div className="flex items-center gap-1 sm:gap-4">
+        <ModeToggle />
+        <HeaderCart />
+      </div>
     </div>
   );
 };
