dfb258eeebcd3fec527a59000dc4868049fc2544
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-09-16 15:43:55 +0545

    implemented the metadata function to genrate metadata for each product

--- app/product/[productid]/page.tsx
+++ app/product/[productid]/page.tsx
@@ -2,6 +2,33 @@ import { getProduct, getProductsByCategory } from "@/actions/products";
 import { ProductDetail } from "@/components/general/ProductDetail";
 import { ProductType } from "@/types/index.types";
 import React from "react";
+import { Metadata } from "next";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ productid: string }>;
+}): Promise<Metadata> {
+  try {
+    const { productid } = await params;
+    const product = await getProduct(productid);
+
+    return {
+      title: `${product.title} - Mini Store`,
+      description: product.description,
+      openGraph: {
+        title: product.title,
+        description: product.description,
+        images: [product.image],
+      },
+    };
+  } catch (error) {
+    console.error("Error fetching product data:", error);
+    return {
+      title: "Product Not Found - Mini Store",
+    };
+  }
+}
 
 const page = async ({ params }: { params: Promise<{ productid: string }> }) => {
   const { productid } = await params;
